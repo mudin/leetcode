1270af4f4cdd3cd096db23eb7112248500f54a49
Mudin Ibrahim <imudin07@gmail.com>
2021-01-28 20:49:52 +0900

    328. Odd Even Linked List

---
+++ problems/328. Odd Even Linked List/solution.py
@@ -0,0 +1,22 @@
+# 328. Odd Even Linked List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head):
+        if not head:
+            return head
+        odd, even = head, head.next
+        evenHead = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head
