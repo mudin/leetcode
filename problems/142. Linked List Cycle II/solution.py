13403d448bc9d5dd7710d55751601879de4ddf2e
Mudin Ibrahim <imudin07@gmail.com>
2021-01-28 20:55:58 +0900

    142. Linked List Cycle II

---
+++ problems/142. Linked List Cycle II/solution.py
@@ -0,0 +1,21 @@
+# 142. Linked List Cycle II
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        while head != slow:
+            slow = slow.next
+            head = head.next
+        return head
