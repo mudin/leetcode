387b183edd168ba3b1cbc935045395ac21c74f27
Mudin Ibrahim <mudin@3i.ai>
2021-02-05 13:37:42 +0900

    594. Longest Harmonious Subsequence

---
+++ problems/594. Longest Harmonious Subsequence/solution.py
@@ -0,0 +1,12 @@
+# 594. Longest Harmonious Subsequence
+
+from collections import Counter
+
+
+class Solution:
+    def findLHS(self, nums):
+        res, counter = 0, Counter(nums)
+        for num, c in counter.items():
+            if num + 1 in counter:
+                res = max(res, counter[num + 1] + c)
+        return res
